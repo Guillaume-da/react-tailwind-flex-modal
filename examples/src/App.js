7feab7c59723eb3e1ef2f46abe4c0bdf78173558
Guillaume-da <singy@hotmail.fr>
2022-09-19 23:36:42 +0200

    examples added: aproval modal and form modal

---
+++ examples/src/App.js
@@ -0,0 +1,130 @@
+import React, {useState} from "react";
+import {Modal} from 'react-tailwind-flex-modal'
+import 'react-tailwind-flex-modal/dist/index.css'
+import './tailwind.css'
+import FormModal from './components/FormModal.jsx'
+import { RiErrorWarningLine } from 'react-icons/ri'
+
+const modalBackground = 'bg-white'
+const darkModalBackground = 'dark:bg-zinc-900'
+const successTitleColor = 'text-lime-600'
+const warningTitleColor = 'text-red-500'
+const darkSuccessTitleColor = 'dark:text-red-500'
+const messageTextColor = 'text-gray-500'
+const aprovalButtonBgColor = 'bg-lime-600'
+const darkAprovalButtonBgColor = 'dark:bg-lime-600'
+const closeButtonBgColor = 'bg-red-600'
+const buttonsTextColor = 'text-white'
+
+const message = "Are you sure ?"
+const warningIcon = <RiErrorWarningLine className="mr-3 text-3xl" />
+
+function App() {
+  const [showModal, setShowModal] = useState(false)
+  const [aprovalMessage, setAprovalMessage] = useState('')
+  const [formModal, setFormModal] = useState(false)
+
+  const [firstName, setFirstName] = useState('')
+	const [lastName, setLastName] = useState('')
+	const [birthdate, setBirthdate] = useState('')
+	const [department, setDepartment] = useState('')
+	const [city, setCity] = useState('')
+	const [startDate, setStartDate] = useState('')
+	const [street, setStreet] = useState('')
+	const [employeeState, setEmployeeState] = useState('')
+	const [zipcode, setZipcode] = useState('')
+  const [closeMessage, setCloseMessage] = useState('')
+  const [warningTitle, setWarningTitle] = useState('')
+
+  const formComponent = 
+	<FormModal 
+		setShowModal={setShowModal} 
+		setFirstName={setFirstName} 
+		firstName={firstName} 
+		lastName={lastName} 
+		setLastName={setLastName} 
+		startDate={startDate} 
+		birthdate={birthdate} 
+		department={department} 
+		city={city}
+		street={street} 
+		zipcode={zipcode} 
+		employeeState={employeeState} 
+		setFormModal={setFormModal}
+		setStartDate={setStartDate}
+		setBirthdate={setBirthdate}
+		setCity={setCity}
+		setStreet={setStreet}
+		setZipcode={setZipcode}
+		setEmployeeState={setEmployeeState}
+		setDepartment={setDepartment}
+	/>
+
+  function handleClose(e) {
+    e.preventDefault()
+    e.stopPropagation()
+    setShowModal(false)
+    setAprovalMessage('')
+  }
+
+  function handleOpenAprovalModal(e){
+    e.preventDefault()
+    e.stopPropagation()
+    setShowModal(true)
+    setCloseMessage('Cancel')
+    setWarningTitle('Warning')
+    setAprovalMessage('Yes, do it!')
+  }
+
+  function handleOpenFormModal(e){
+    e.preventDefault()
+    e.stopPropagation()
+    setShowModal(true)
+    setFormModal(true)
+  }
+
+  return (
+    <div className="dark:bg-zinc-800 min-h-screen">
+      
+    <div className="mx-auto rounded-2xl bg-white pb-2 shadow-xl max-w-screen-xl dark:bg-zinc-900">
+    {showModal ? 
+        <Modal 
+        setShowModal={setShowModal} // add this for all modals
+        showModal={showModal} // add this for all modals
+        aprovalMessage={aprovalMessage} // confirm button, add this for Aproval Modal
+        message={message} 
+        closeMessage={closeMessage} 
+        handleClose={handleClose}
+        modalBackground={modalBackground}
+        darkModalBackground={darkModalBackground}
+        successTitleColor={successTitleColor}
+        warningTitleColor={warningTitleColor}
+        darkSuccessTitleColor={darkSuccessTitleColor}
+        messageTextColor={messageTextColor}
+        aprovalButtonBgColor={aprovalButtonBgColor}
+        darkAprovalButtonBgColor={darkAprovalButtonBgColor}
+        closeButtonBgColor={closeButtonBgColor}
+        buttonsTextColor={buttonsTextColor}
+        formComponent={formComponent} // add this for Form Modal
+        formModal={formModal} // add this for Form Modal
+        warningTitle={warningTitle} 
+        warningIcon={warningIcon}
+    />
+    : 
+    <div></div>  
+    }
+    
+				<div className="horizontal xl:container mt-5 flex ">
+          <div className="my-10 p-2 lg:p-10">    
+            <button onClick={(e)=>handleOpenAprovalModal(e)} className="bg-cyan-700 select-none cursor-pointer rounded-lg border-2 border-gray-200 py-3 px-6 font-bold text-gray-200 transition-colors duration-200 ease-in-out peer-checked:bg-gray-200 peer-checked:text-gray-900 peer-checked:border-gray-200 ">Aproval Modal</button>
+					</div>
+          <div className="my-10 p-2 lg:p-10">    
+            <button onClick={(e)=>handleOpenFormModal(e)} className="bg-cyan-700 select-none cursor-pointer rounded-lg border-2 border-gray-200 py-3 px-6 font-bold text-gray-200 transition-colors duration-200 ease-in-out peer-checked:bg-gray-200 peer-checked:text-gray-900 peer-checked:border-gray-200 ">Form Modal</button>
+					</div>
+				</div> 
+		</div> 
+    </div>    
+  )
+}
+
+export default App;
---
+++ examples/src/components/FormModal.jsx
@@ -0,0 +1,197 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable react/prop-types */
+import React from 'react'
+
+function FormModal(props) {
+
+	const form = {
+		firstName: props.firstName,
+		lastName: props.lastName,
+		startDate: props.startDate,
+		department: props.department,
+		birthdate: props.birthdate,
+		street: props.street,
+		city: props.city,
+		state: props.employeeState,
+		zipcode: props.zipcode
+	}
+
+	function handleFormSending(e) {
+		// eslint-disable-next-line no-unused-expressions
+		e.preventDefault
+		console.log(form) 
+		props.setShowModal(false) 
+		props.setFormModal(false)
+	}
+
+	return (
+		<div className="w-full max-w-lg">
+			<div className="flex flex-wrap -mx-3 mb-2">
+				<div className="w-full md:w-1/2 px-3 ">
+					<label className="block uppercase tracking-wide text-gray-600 dark:text-gray-300 text-xs font-bold mb-2" htmlFor="grid-first-name">
+                    First Name
+					</label>
+					<input 
+						className="appearance-none block w-full bg-gray-200 text-gray-600 dark:text-gray-400 rounded py-3 px-4 leading-tight border border-gray-200 dark:border-zinc-800 focus:outline-none focus:bg-white dark:bg-zinc-800" 
+						id="grid-first-name" 
+						defaultValue={props.firstName}
+						type="text" 
+						placeholder={props.firstName} 
+						onChange={(e) => console.log(e.target.value)}
+					/>
+				</div>
+				<div className="w-full md:w-1/2 px-3">
+					<label className="block uppercase tracking-wide text-gray-600 dark:text-gray-300 text-xs font-bold mb-2" htmlFor="grid-last-name">
+                    Last Name
+					</label>
+					<input 
+						className="appearance-none block w-full bg-gray-200 text-gray-600 dark:text-gray-400 border border-gray-200 dark:border-zinc-800 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 dark:bg-zinc-800" 
+						id="grid-last-name" 
+						type="text" 
+						defaultValue={props.lastName}
+						placeholder={props.lastName} 
+						onChange={(e) => console.log(e.target.value)}
+					/>
+				</div>
+			</div>
+			<div className="w-full flex-1">
+				<div className="h-6 text-gray-600 dark:text-gray-300 text-xs font-bold uppercase leading-8 mb-2" >
+                Date of birth
+				</div>
+				<div className="my-2 flex rounded border border-gray-200 dark:border-zinc-800 bg-gray-200 text-gray-700 dark:bg-zinc-800 p-1" >
+					<input
+						defaultValue={props.birthdate}
+						placeholder={props.birthdate}
+						onChange={(e) => console.log(e.target.value)}
+						name="birthdate"
+						type="date"
+						className="w-full appearance-none p-1 px-2 bg-gray-200 text-gray-600 dark:text-gray-400 outline-none dark:bg-zinc-800"
+					/>
+				</div>
+			</div>
+			<div className="flex flex-wrap -mx-3 mt-4">
+				<div className="w-full md:w-1/2 px-3 mb-2">
+					<label className="block uppercase tracking-wide text-gray-600 dark:text-gray-300 text-xs font-bold mb-2" htmlFor="grid-state">
+                    Department
+					</label>
+					<div className="relative">
+						<select 
+							aria-label="Select"
+							className="block appearance-none w-full bg-gray-200 dark:bg-zinc-800 border border-gray-200 dark:border-zinc-800 text-gray-600 dark:text-gray-400 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
+							id="grid-state"
+							name="employeeState"
+							value={props.department}
+							placeholder={props.department} 
+							onChange={(e) => console.log(e.target.value)}
+						>	
+						<option value=''>Select a value</option>
+							
+						</select>
+						<div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700 dark:text-gray-300">
+							<svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+						</div>
+					</div>
+				</div>
+				<div className="w-full md:w-1/2 px-3">
+					<label className="block uppercase tracking-wide text-gray-600 dark:text-gray-300 text-xs font-bold mb-2" htmlFor="grid-last-name">
+                    City
+					</label>
+					<input 
+						className="appearance-none block w-full bg-gray-200 dark:bg-zinc-800 text-gray-600 dark:text-gray-400 border border-gray-200 dark:border-zinc-800 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
+						id="grid-last-name" 
+						type="text" 
+						defaultValue={props.city}
+						placeholder={props.city} 
+						onChange={(e) => props.setCity?.(e.target.value)}
+					/>
+				</div>
+			</div>
+			<div className="w-full flex-1">
+				<div className="h-6 text-gray-600 dark:text-gray-300 text-xs font-bold uppercase leading-8" >
+                Start Date
+				</div>
+				<div className="my-2 flex rounded border border-gray-200 dark:border-zinc-800 bg-gray-200 dark:bg-zinc-800 p-1" >
+					<input
+						defaultValue={props.startDate}
+						name="birthdate"
+						type="date"
+						onChange={(e) => props.setStartDate?.(e.target.value)}
+						className="w-full appearance-none p-1 px-2 bg-gray-200 text-gray-600 dark:text-gray-400 outline-none dark:bg-zinc-800"
+					/>
+				</div>
+			</div>
+			<div className="flex flex-wrap -mx-3 mb-3 mt-4">
+				<div className="w-full px-3">
+					<label className="block uppercase tracking-wide text-gray-600 dark:text-gray-300 text-xs font-bold mb-2" htmlFor="grid-password">
+                    Street
+					</label>
+					<input 
+						className="appearance-none block w-full bg-gray-200 dark:bg-zinc-800 text-gray-600 dark:text-gray-400 border border-gray-200 dark:border-zinc-800 rounded py-3 px-4 mb-2 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
+						id="grid-password" 
+						type="text" 
+						defaultValue={props.street}
+						placeholder={props.street}
+						onChange={(e) => props.setStreet?.(e.target.value)}
+					/>
+				</div>
+			</div>
+			<div className="flex flex-wrap -mx-3 mb-2">
+				<div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+					<label className="block uppercase tracking-wide text-gray-600 dark:text-gray-300 text-xs font-bold mb-2" htmlFor="grid-state">
+                    State
+					</label>
+					<div className="relative">
+						<select 
+							aria-label="Select"
+							className="block appearance-none w-full bg-gray-200 dark:bg-zinc-800 border border-gray-200 dark:border-zinc-800 text-gray-600 dark:text-gray-400 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
+							id="grid-state"
+							name="employeeState"
+							value={props.employeeState}
+							placeholder={props.employeeState} 
+							onChange={(e) => props.setEmployeeState?.(e.target.value)}
+						>
+							{props.statesList?.map((state, index) => {
+								return <option key={index} value={state.name} >{state.name} </option>
+							})}
+						</select>
+						<div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700 dark:text-gray-300">
+							<svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+						</div>
+					</div>
+				</div>
+				<div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+					<label className="block uppercase tracking-wide text-gray-600 dark:text-gray-300 text-xs font-bold mb-2" htmlFor="grid-zip">
+                    Zip
+					</label>
+					<input 
+						className="appearance-none block w-full bg-gray-200 dark:bg-zinc-800 text-gray-600 dark:text-gray-300 border border-gray-200 dark:border-zinc-800 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
+						id="grid-zip" 
+						type="text" 
+						defaultValue={props.zipcode}
+						placeholder={props.zipcode} 
+						onChange={(e) => props.setZipcode?.(e.target.value)}
+					/>
+				</div>
+            
+			</div>
+			<div className="flex flex-wrap -mx-3 mt-6 mb-2">
+				<div className="w-full flex justify-between px-3">
+					<button 
+						className="w-5/12 bg-rose-600 hover:bg-rose-700 text-white font-bold py-2 px-4 rounded"
+						onClick={() => {props.setShowModal(false), props.setFormModal(false)}}
+					>
+                    Cancel
+					</button>
+					<button 
+						className="w-5/12 bg-lime-600 hover:bg-lime-700 text-white font-bold py-2 px-4 rounded"
+						onClick={(e) => console.log(form)}
+					>
+                    Save
+					</button>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default FormModal
