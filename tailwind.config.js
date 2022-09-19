27ae6f71314445d682fe301e1eb7bb7086a4d090
Guillaume-da <singy@hotmail.fr>
2022-09-20 01:19:01 +0200

    fade-in-down animation added to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,26 @@ module.exports = {
     enabled: process.env.NODE_ENV === 'publish',
     content: ['./src/**/*.{js,jsx,ts,tsx}']
   },
+  darkMode: 'class',
   content: [],
   theme: {
-    extend: {}
+    extend: {
+      animation: {
+        'fade-in-down': 'fade-in-down 0.5s ease-out'
+      },
+      keyframes: {
+        'fade-in-down': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(-25px)'
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)'
+          }
+        }
+      }
+    }
   },
   plugins: []
 }
